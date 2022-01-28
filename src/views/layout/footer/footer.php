6a40d15e6112b8adcba7042dc32620893b91028d
Sergei MILITER <militer@htmlcssjs.pro>
2022-01-28 17:49:00 +1000

    UPDATE layout

---
+++ src/views/layout/footer/footer.php
@@ -0,0 +1,18 @@
+<footer id="footer" class="footer">
+    <div class="footer__wrapper">
+        <div class="footer__logo">Сергей MILITER</div>
+        <nav class="footer__nav">
+            <a href="#works" class="footer__link">примеры работ</a>
+            <a href="#skills" class="footer__link">навыки</a>
+            <a href="#about" class="footer__link">обо мне</a>
+            <a href="#reviews" class="footer__link">отзывы</a>
+            <a href="#contacts" class="footer__link">контакты</a>
+        </nav>
+    </div>
+</footer>
+
+<footer id="footer" class="footer">
+    <div class="footer__top"></div>
+    <div class="footer__wrapper"></div>
+    <div class="footer__bottom"></div>
+</footer>
---
+++ src/views/layout/footer/multyFooter.php
@@ -0,0 +1,11 @@
+<div class="footer__top">
+    <?php $this->getSection('yashare'); ?>
+</div>
+
+<div class="footer__bottom">
+    <p>
+        &copy; 2021
+    </p>
+</div>
+
+<?php
